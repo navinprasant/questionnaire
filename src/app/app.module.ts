5bafbd84bde654bedcbb5e2d308f75fadeeea961
prashant <prashant@diro.io>
2020-02-25 01:56:47 +0530

    removing d3 chart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarChartComponent } from 'angular-d3-charts';
+// import { BarChartComponent } from 'angular-d3-charts';
 import { QuestionsComponent } from './questions/questions.component'; // this is needed!
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarChartComponent,
+    // BarChartComponent,
     QuestionsComponent
   ],
   imports: [
